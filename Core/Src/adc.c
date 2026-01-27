aa6eff8a1036d01b38c12c32575ab300fc50491e
13112442872 <891771212@qq.com>
2026-01-27 15:44:31 +0800

    增加过流阈值动态填充

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -450,7 +450,26 @@ void adc_hvli_input_conv(int num)  // 直流高压输入电压,低端电流
 
     if( !adc_dc_lcur_offset ) {
         adc_dc_lcur_offset = (uint16_t)adc_dc_lcur_avg;
+    #if HARDWARE_VERSION_CODE == HW_VER_2_0_INT
+        #define Over_Current_mA         80      //限流
+        #define Sampling_Resistance_mR  110     //采样电阻
+        #define OpAmp_Gain              101     //运算放大倍数
+        #define Vdd_V                   MCU_VDD_GAIN_10X // MCU供电电压
+
+        uint16_t vol_dc_lcur = adc_dc_lcur_offset * Vdd_V*100 / 4096; // 总共放大1000倍
+        // printf("vol_dc_lcur:%d \r\n", vol_dc_lcur);
+        uint16_t over_current_threshold = Over_Current_mA * Sampling_Resistance_mR * OpAmp_Gain / 1000; // 总共放大1000倍
+        // printf("over_current_threshold:%d \r\n", over_current_threshold);
+        uint16_t register_value = (vol_dc_lcur + over_current_threshold) * 255 / Vdd_V / 100;
+        if( register_value > 0xFF ) {
+            register_value = 0xFF;
+        }
+
+        COMP_SetCrv(COMP_CRV_SRC_VDDA, register_value);
+        printf("adc_dc_lcur_offset:%d register_value:%d Over_Current_mA:%d\r\n", adc_dc_lcur_offset, register_value, Over_Current_mA);
+    #else
         printf("adc_dc_lcur_offset:%d\r\n", adc_dc_lcur_offset);
+    #endif
     }
     adc_dc_lcur_avg -= adc_dc_lcur_offset;
 }
